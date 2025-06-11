587b3f5b2419ce61098b37a1ec83ad455125dd6d
Irawan Kilmer <irawankillmer@gmail.com>
2025-06-11 11:45:30 +0700

    CORS Middleware

---
+++ README.md
@@ -0,0 +1,7 @@
+## Update
+ - CORS Middleware
+## Target
+ - middleware role
+ - token_version(token sekali pakai jika user sudah logout atau login ulang)
+ - rate limit
+ - refresh token
--- internal/routes/router.go
+++ internal/routes/router.go
@@ -3,12 +3,14 @@ package routes
 import (
 	"be-blog/internal"
 	"be-blog/internal/handler"
+	"be-blog/internal/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter(r *gin.Engine, app *internal.AppContainer) {
 	authHandler := handler.NewAuthHandler(app.AuthService)
 
+	r.Use(middleware.CORSMiddleware())
 	api := r.Group("/api")
 	api.POST("/login", authHandler.Login)
 }
